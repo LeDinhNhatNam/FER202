6bbe57b3636ca0314a72dc7be05ee255971b54e8
Le Dinh Nhat Nam <namldnde180005@fpt.edu.vn>
2025-09-26 10:38:34 +0700

    chỉnh sửa slot5 React App

--- slot5/ex2/src/App.js
+++ slot5/ex2/src/App.js
@@ -1,10 +1,10 @@
-import Exercise1 from './components/exercise1';
+import Exercise2 from './components/exercise2';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Exercise1 />
+      <Exercise2 />
     </div>
   );
 }
